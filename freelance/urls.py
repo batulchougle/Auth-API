31706b3b28bb2f9885251ffc068dd139f9f921bf
batulchougle <batulchougle@gmail.com>
2024-09-16 15:10:49 +0530

    Update urls.py

--- freelance/urls.py
+++ freelance/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/auth/' , include("account.urls"))
+    path('api/v1/auth/' , include("account.urls")),
+    path('api/v1/auth/' , include("social_auth.urls")),
 ]
